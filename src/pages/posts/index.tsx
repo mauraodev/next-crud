1b0bec6104853f61eb06b46e8753c2b0477e391d
Mauro Nunes <mauro.nunes@ikatec.com.br>
2025-09-15 09:41:27 -0300

    header

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/Layouts";
+
+const Posts = () => {
+    return (
+      <Layout titulo={"Posts"}>
+        <>
+          <h2>Testando</h2>
+        </>
+      </Layout>
+    );
+  }
+
+  export default Posts;
